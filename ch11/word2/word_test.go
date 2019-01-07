554d285421fba77122edf340ad6d49827377362b
huangyao <505757398@163.com>
2019-01-07 21:38:52 +0800

    cp11

---
+++ ch11/word2/word_test.go
@@ -0,0 +1,41 @@
+package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+// randomPalindrome returns a palindrome whose length and contetns
+// are derived frome the pseudo-random number generator rng.
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // random length up to 24
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000)) // randome rune up to `\u0999`
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	// Initialize a pseudo-random number generagor.
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a cannal: Pannama")
+	}
+}
